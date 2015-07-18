4b98b5a97b69b8038253587c02113136b380579f
Maciek Fijalkowski <macfij7@wp.pl>
2015-07-18 17:06:23 +0200

    [lab6] implement net/output.c

--- net/output.c
+++ net/output.c
@@ -6,8 +6,14 @@ void
 output(envid_t ns_envid)
 {
 	binaryname = "ns_output";
+	int val;
 
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	while (1) {
+		val = ipc_recv(0, &nsipcbuf, 0);
+		if (val == NSREQ_OUTPUT)
+			sys_tx_data(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len);
+	}
 }
